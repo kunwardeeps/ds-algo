f0f20b2300e5a46e43f137ced79bd116f69e43e7
Kunwardeep Singh <kunwardeep.singh@gmail.com>
2017-08-15 12:20:10 +0530

    Graph traversals

--- DSAlgo/src/com/hackerrank/Main.java
+++ DSAlgo/src/com/hackerrank/Main.java
@@ -1,4 +1,4 @@
-package hackerrank;
+package com.hackerrank;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
--- DSAlgo/src/com/hackerrank/Solution_filter.java
+++ DSAlgo/src/com/hackerrank/Solution_filter.java
@@ -1,4 +1,4 @@
-package hackerrank;
+package com.hackerrank;
 import java.io.*;
 import java.util.*;
 import java.text.*;
